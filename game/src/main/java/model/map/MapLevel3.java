8c9f25d15877577636bdda44bc272dabfe4148b9
MinaAbbspr <Mina4434abbas@gmail.com>
2024-07-06 19:56:29 +0330

    level3 graphic part1 added

---
+++ game/src/main/java/model/map/MapLevel3.java
@@ -0,0 +1,24 @@
+package model.map;
+
+import java.util.ArrayList;
+
+public class MapLevel3 extends Map{
+
+    public MapLevel3() {
+        super(new Coordinate(680,725), 9, 450);
+        ArrayList <Coordinate> tower = new ArrayList<>();
+        tower.add(new Coordinate(1302,383));
+        tower.add(new Coordinate(1342,515));
+        tower.add(new Coordinate(355,396));
+        tower.add(new Coordinate(287,547));
+        tower.add(new Coordinate(568,453));
+        tower.add(new Coordinate(1174,604));
+        tower.add(new Coordinate(1124,424));
+        tower.add(new Coordinate(454,319));
+        super.setTowersCoordinate(tower);
+
+        ArrayList<Coordinate> way = new ArrayList<>();
+        super.setWay(way);
+
+    }
+}
